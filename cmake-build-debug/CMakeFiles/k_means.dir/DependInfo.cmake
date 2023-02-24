6d39a608228ce9e72bbd2d74392680b580d28f2c
Xieyu Wang <varianovsky@outlook.com>
2023-02-24 12:36:48 +0300

    ..

---
+++ cmake-build-debug/CMakeFiles/k_means.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/CPP_Codes/k_means/IOController.cpp" "E:/CPP_Codes/k_means/cmake-build-debug/CMakeFiles/k_means.dir/IOController.cpp.obj"
+  "E:/CPP_Codes/k_means/KMeans.cpp" "E:/CPP_Codes/k_means/cmake-build-debug/CMakeFiles/k_means.dir/KMeans.cpp.obj"
+  "E:/CPP_Codes/k_means/Point.cpp" "E:/CPP_Codes/k_means/cmake-build-debug/CMakeFiles/k_means.dir/Point.cpp.obj"
+  "E:/CPP_Codes/k_means/main.cpp" "E:/CPP_Codes/k_means/cmake-build-debug/CMakeFiles/k_means.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
